b1c97927129c3e1a3a9cd7fc1a58cbd867cee88e
imam.arifin02 <imam.arifin052@gmail.com>
2022-06-26 23:16:29 +0700

    Tantangan Pengenalan React Done!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-
+import Header from './components/Header';
+import Contact from './components/Contact';
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header/>
+      {contacts.map((value, index) => {
+        return <Contact key={index} data={value}/>
+      })
+      }
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
-
-const Contact = () => {
+import './Contact.css';
+const Contact = ({data}) => {
+    const {name, phone, email, photo: avatar} = data;
     return (
-        <></>
+        <>
+            <div className="contact">
+                <div className="contact-avatar">
+                    <img src={avatar} alt="avatar" />
+                </div>
+                <div className="contact-info">
+                    <h2>{name}</h2>
+                    <p>{phone}</p>
+                    <p>{email}</p>
+                </div>
+            </div>
+        </>
     )
 }
 
